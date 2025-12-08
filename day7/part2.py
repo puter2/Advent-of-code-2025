a77dbed1dbc0aed139088a11a1fba7a8ebdc52ab
puter <piotrzajac4@gmail.com>
2025-12-08 01:55:35 +0100

    day7 complete

--- day7/part2.py
+++ day7/part2.py
@@ -1,34 +1,39 @@
 from reader import day7
 
+def advance_until_next_splitter(splitters, node, height):
+    while node not in splitters and node[1] < height:
+        node = (node[0], node[1] + 1)
+    return node
+
 file = 'input.txt'
 
 splitters, start, height = day7(file)
 
-# rekurencja
-paths = []
-def find_paths(start_path:list, next_step:tuple):
-    # input()
-    next_height = next_step[1]
-    print(next_height, next_step)
-    if next_step in splitters:
-        left = (next_step[0] - 1, next_height + 1)
-        right = (next_step[0] + 1, next_height + 1)
-        left_path = [*start_path, left]
-        right_path = [*start_path, right]
-        find_paths(left_path, left)
-        find_paths(right_path, right)
-    else:
-        global height
-        if next_height >= height:
-            global paths
-            paths.append(start_path.copy())
-        else:
-            next_next_step = (next_step[0], next_height + 1)
-            down_path = [*start_path]
-            find_paths(down_path, next_next_step)
+'''
+idea:
+label each splitter with number of paths to the bottom from that splitter starting from bottom
+splitters at the bottom will have only two paths
+each higher splitter must check, if there's a next splitter below to the left and to the right,
+if there is a splitter to the below-left then add it's number of paths to current splitter label, otherwise add 1
+similarly with below-right
+use the same rule with start node
+label on the start node is the total number of paths
+'''
 
-next_step = (start[0], 1)
-find_paths([start], next_step)
-for path in paths:
-    print(path)
-print(len(paths))
+number_of_paths = {
+    start : 0,
+}
+
+splitters_sorted = sorted({*splitters, start}, key = lambda a: a[1], reverse=True)
+
+for splitter in splitters_sorted:
+    curr_height = splitter[1]
+    left = (splitter[0] - 1, curr_height + 1)
+    right = (splitter[0] + 1, curr_height + 1)
+    left = advance_until_next_splitter(splitters, left, height)
+    right = advance_until_next_splitter(splitters, right, height)
+    number_of_paths[splitter] = 0
+    number_of_paths[splitter] += 1 if left[1]>=height else number_of_paths[left]
+    number_of_paths[splitter] += 1 if right[1] >= height else number_of_paths[right]
+
+print(number_of_paths[start])
